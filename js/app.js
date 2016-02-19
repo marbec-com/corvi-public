7cc937f13b913fc6f9fec942f7ace1ae4a8449a8
Mark-Johannes Becker <mjb@marb.ec>
2016-02-19 22:18:43 +0100

    Angular added, Routing added, Category fetches

---
+++ js/app.js
@@ -0,0 +1,59 @@
+var corviApp = angular.module('corviApp', ['ngRoute']);
+
+corviApp.config(function($routeProvider) {
+	$routeProvider
+	.when('/', {
+		templateUrl: 'sites/study_boxes.html',
+		controller: 'studyBoxController',
+		navActive: 'study'
+	})
+	.when('/manage', {
+		templateUrl: 'sites/manage.html',
+		controller: 'mainController',
+		navActive: 'manage'
+	})
+	.when('/discover', {
+		templateUrl: 'sites/discover.html',
+		controller: 'mainController',
+		navActive: 'discover'
+	})
+	.when('/stats', {
+		templateUrl: 'sites/stats.html',
+		controller: 'mainController',
+		navActive: 'stats'
+	})
+	.when('/settings', {
+		templateUrl: 'sites/settings.html',
+		controller: 'mainController',
+		navActive: 'settings'
+	});
+});
+
+corviApp.controller('studyBoxController', function($scope, $http, $log) {
+	$scope.categories = {};
+	$http.get("/api/categories").then(function(res) {
+		if(res.status === 200) {
+			$scope.categories = res.data;
+			$log.debug(res.data);
+		}else{
+			$log.error(res);
+		}
+		
+	});
+});
+
+corviApp.controller('mainController', function($scope) {
+	$scope.message = 'Hello Angular!';
+});
+
+corviApp.directive('mainNavigation', function() {
+	return {
+		retrict: 'E',
+		templateUrl: 'partials/mainNavigation.html',
+		controller: 'mainNavigationController'
+	}
+});
+
+corviApp.controller('mainNavigationController', function($scope, $route) {
+	$scope.$route = $route;
+});

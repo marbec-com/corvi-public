f16fae5b5ab6c68fcd8d5c210fcb8da3ff60bf2b
Mark-Johannes Becker <mjb@marb.ec>
2016-02-20 00:00:00 +0100

    Question workflow finished.

--- js/app.js
+++ js/app.js
@@ -7,9 +7,14 @@ corviApp.config(function($routeProvider) {
 		controller: 'studyBoxController',
 		navActive: 'study'
 	})
+	.when('/study/:box/finished', {
+		templateUrl: 'sites/study_finished.html',
+		controller: 'studyFinishedController',
+		navActive: 'study'
+	})
 	.when('/study/:box', {
 		templateUrl: 'sites/study_question.html',
-		controller: 'mainController',
+		controller: 'studyQuestionController',
 		navActive: 'study'
 	})
 	.when('/manage', {
@@ -31,29 +36,102 @@ corviApp.config(function($routeProvider) {
 		templateUrl: 'sites/settings.html',
 		controller: 'mainController',
 		navActive: 'settings'
+	}).otherwise({
+		redirectTo: '/'
 	});
 });
 
 corviApp.controller('studyBoxController', function($scope, $http, $log) {
 	$scope.categories = {};
 	$http.get("/api/categories").then(function(res) {
-		if(res.status === 200) {
-			$scope.categories = res.data;
-			$log.debug(res.data);
-		}else{
-			$log.error(res);
-		}
-		
-		Object.keys($scope.categories).forEach(function(key) {
-			$http.get("/api/category/" + $scope.categories[key].ID + "/boxes").then(function(res) {
-				if(res.status == 200) {
-					$scope.categories[key].Boxes = res.data;
-				}
-				$log.debug($scope.categories);
-			});
-		});
-		
+		$scope.categories = res.data;
+		Object.keys($scope.categories).forEach(loadBoxForCategory);
+	}, function(res) {
+		$log.error(res);
 	});
+	
+	function loadBoxForCategory(catID) {
+		$http.get("/api/category/" + $scope.categories[catID].ID + "/boxes").then(function(res) {
+			$scope.categories[catID].Boxes = res.data;
+		}, function(res) {
+			$log.error(res);
+		});
+	}
+	
+});
+
+corviApp.controller('studyFinishedController', function($scope, $routeParams, $location, $log, $http) {
+	
+	var id = parseInt($routeParams.box, 10)
+	if (isNaN(id)) {
+		$log.error("Invalid ID!");
+	}
+	
+	$scope.box = {};
+	
+	$http.get("/api/box/"+id+"/").then(function(res) {
+		$scope.box = res.data;
+	}, function(res) {
+		$log.error(res);
+		$location.path("/");
+	});
+	
+	$scope.getBack = function() {
+		$location.path("/");
+	};
+	
+});
+
+corviApp.controller('studyQuestionController', function($scope, $routeParams, $log, $location, $http) {
+	
+	var boxId = parseInt($routeParams.box, 10)
+	if (isNaN(boxId)) {
+		$log.error("Invalid ID!");
+	}
+
+	loadNewQuestion(boxId);	
+	
+	$scope.showSolution = function() {
+		$scope.answered = true;	
+	};
+	
+	$scope.giveCorrectAnswer = function() {
+		// Save correct answer
+		$http.put("/api/question/"+$scope.question.ID+"/giveCorrectAnswer").then(function(res) {
+			// Load next question
+			loadNewQuestion(boxId);	
+		}, function(res) {
+			$log.error(res);
+			$location.path("/");
+		});
+	};
+	
+	$scope.giveWrongAnswer = function() {
+		// Save correct answer
+		$http.put("/api/question/"+$scope.question.ID+"/giveWrongAnswer").then(function(res) {
+			// Load next question
+			loadNewQuestion(boxId);	
+		}, function(res) {
+			$log.error(res);
+			$location.path("/");
+		});
+	};
+	
+	function loadNewQuestion(boxID) {
+		$scope.answered = false;
+		$scope.question = {};
+		
+		$http.get("/api/box/"+boxID+"/getQuestionToLearn").then(function(res) {
+			if(res.status == 200) {
+				$scope.question = res.data;
+			}else{ // No more questions
+				$location.path("/study/"+boxID+"/finished");
+			}
+		}, function(res) { // Error
+			$log.error(res);
+			$location.path("/");
+		});
+	};
 });
 
 corviApp.controller('mainController', function($scope) {
